42704357f19537bd4aa64a1073cc4fff4898d460
sasonhe <642139009@qq.com>
2017-10-03 13:01:39 +0800

    add

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
-// "use strict";
+"use strict";
 import Vue from 'vue';
-import VueRouter from 'vue-router'
-import VueResource from 'vue-resource'
+import VueRouter from 'vue-router';
+import VueResource from 'vue-resource';
 import App from './App.vue';
 import routes from './router.config'
 import Mint from 'mint-ui';
@@ -10,13 +10,13 @@ import './common/reset.css';
 
 Vue.use(Mint);
 
-Vue.use(VueRouter)
-Vue.use(VueResource)
+Vue.use(VueRouter);
+Vue.use(VueResource);
 
 
 var router = new VueRouter({
     routes
-})
+});
 
 new Vue({
     el : '#app',
