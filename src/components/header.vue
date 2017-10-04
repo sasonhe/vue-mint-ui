69a585c3de472a0c66745b068177b2b82f24d6e5
sasonhe <642139009@qq.com>
2017-10-04 17:21:45 +0800

    add

---
+++ src/components/header.vue
@@ -0,0 +1,20 @@
+<template>
+<div class="">
+    <mt-header title="long long long long title">
+      <router-link to="/" slot="left">
+        <mt-button icon="back">返回</mt-button>
+      </router-link>
+      <mt-button icon="more" slot="right"></mt-button>
+    </mt-header>
+</div>
+</template>
+<script>
+export default{
+    components:{
+
+    }
+}
+</script>
+<style scoped>
+
+</style>
---
+++ src/components/swipe.vue
@@ -0,0 +1,41 @@
+<template>
+    <mt-swipe :auto="4000" class="wrap">
+      <mt-swipe-item class="ban1">1</mt-swipe-item>
+      <mt-swipe-item class="ban2">2</mt-swipe-item>
+      <mt-swipe-item class="ban3">3</mt-swipe-item>
+    </mt-swipe>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+      }
+    },
+    created() {
+    },
+    methods: {
+    }
+  }
+</script>
+
+<style scoped>
+.wrap{
+    height: 100px;
+    text-align: center;
+}
+.ban1,.ban2,.ban3{
+    width: 100%;
+    height: 100px;
+    line-height: 100px;
+}
+.ban1{
+    background: yellow;
+}
+.ban2{
+    background: red;
+}
+.ban3{
+    background: #ccc;
+}
+</style>
