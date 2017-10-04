61edd22937e7ecb5e781efee1737925789308ed1
sasonhe <642139009@qq.com>
2017-10-04 17:20:24 +0800

    add

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,16 @@
 <template>
 <div>
     <v-header></v-header>
+    <swipe></swipe>
 </div>
 </template>
 <script>
 import header from './components/header.vue'
-
+import swipe from './components/swipe.vue'
 export default{
     components:{
-        'v-header':header
+        'v-header':header,
+        swipe
     }
 }
 </script>
