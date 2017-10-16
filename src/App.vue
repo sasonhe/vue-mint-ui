7a724fa20d3c138786c7c5e6a5e37c32abca389b
sasonhe <642139009@qq.com>
2017-10-16 20:53:59 +0800

    add

--- src/App.vue
+++ src/App.vue
@@ -2,15 +2,18 @@
 <div>
     <v-header></v-header>
     <swipe></swipe>
+    <tabar></tabar>
 </div>
 </template>
 <script>
 import header from './components/header.vue'
 import swipe from './components/swipe.vue'
-export default{
-    components:{
-        'v-header':header,
-        swipe
+import tabar from './components/tabar.vue'
+export default {
+    components: {
+        'v-header': header,
+        swipe,
+        tabar
     }
 }
 </script>
--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import Vue from 'vue';
 import VueRouter from 'vue-router';
 import VueResource from 'vue-resource';
 import App from './App.vue';
-import routes from './router.config'
+import routes from './router.config';
 import Mint from 'mint-ui';
 import 'mint-ui/lib/style.css';
 import './common/reset.css';
