b41b9e73070123f870ef8e6c337877abc8372e4e
sasonhe <642139009@qq.com>
2017-10-16 21:26:59 +0800

    add

--- src/components/tabar.vue
+++ src/components/tabar.vue
@@ -1,37 +1,69 @@
 <template>
-<div class="page-navbar">
-    <div class="page-title">Navbar</div>
-    <mt-navbar class="page-part" v-model="selected">
-        <mt-tab-item id="1">选项一</mt-tab-item>
-        <mt-tab-item id="2">选项二</mt-tab-item>
-        <mt-tab-item id="3">选项三</mt-tab-item>
-    </mt-navbar>
+<div class="page-tabbar">
+    <div class="page-wrap">
+        <div class="page-title">Tabbar</div>
+        <div>
+            <mt-cell class="page-part" title="当前选中" :value="selected" />
+        </div>
 
-    <div>
-        <mt-cell class="page-part" title="当前选中">{{ selected }}</mt-cell>
+        <!-- tabcontainer -->
+        <mt-tab-container class="page-tabbar-container" v-model="selected">
+            <mt-tab-container-item id="外卖">
+                <mt-cell :title="餐厅" />
+            </mt-tab-container-item>
+            <mt-tab-container-item id="订单">
+                <mt-cell :title="订单" />
+            </mt-tab-container-item>
+            <mt-tab-container-item id="发现">
+                <mt-cell :title="发现" />
+            </mt-tab-container-item>
+            <mt-tab-container-item id="我的">
+                123
+                <router-link to="/">
+                    <!-- button -->
+                    <mt-button type="danger" size="large">退出</mt-button>
+                </router-link>
+            </mt-tab-container-item>
+        </mt-tab-container>
     </div>
 
-    <mt-tab-container v-model="selected">
-        <mt-tab-container-item id="1">
-            <mt-cell v-for="n in 10" :title="'内容 ' + n" />
-        </mt-tab-container-item>
-        <mt-tab-container-item id="2">
-            <mt-cell v-for="n in 4" :title="'测试 ' + n" />
-        </mt-tab-container-item>
-        <mt-tab-container-item id="3">
-            <mt-cell v-for="n in 6" :title="'选项 ' + n" />
-        </mt-tab-container-item>
-    </mt-tab-container>
+    <mt-tabbar v-model="selected" fixed>
+        <mt-tab-item id="外卖">
+            <img slot="icon" src="../assets/images/vue.png"> 外卖
+        </mt-tab-item>
+        <mt-tab-item id="订单">
+            <img slot="icon" src="../assets/images/vue.png"> 订单
+        </mt-tab-item>
+        <mt-tab-item id="发现">
+            <img slot="icon" src="../assets/images/vue.png"> 发现
+        </mt-tab-item>
+        <mt-tab-item id="我的">
+            <img slot="icon" src="../assets/images/vue.png"> 我的
+        </mt-tab-item>
+    </mt-tabbar>
 </div>
 </template>
 
 <script>
 export default {
-    name: 'page-navbar',
+    name: 'page-tabbar',
     data() {
         return {
-            selected: '1'
+            selected: '外卖'
         };
     }
 };
 </script>
+
+<style>
+.page-tabbar {
+    overflow: hidden;
+    height: 100vh;
+}
+
+.page-wrap {
+    overflow: auto;
+    height: 100%;
+    padding-bottom: 100px;
+}
+</style>
