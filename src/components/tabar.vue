ba69d427d3c31f8a530bf269d7ac860fe429abae
sasonhe <642139009@qq.com>
2017-10-17 20:47:56 +0800

    delete

--- src/components/tabar.vue
+++ src/components/tabar.vue
@@ -8,22 +8,7 @@
 
         <!-- tabcontainer -->
         <mt-tab-container class="page-tabbar-container" v-model="selected">
-            <mt-tab-container-item id="外卖">
-                <mt-cell :title="餐厅" />
-            </mt-tab-container-item>
-            <mt-tab-container-item id="订单">
-                <mt-cell :title="订单" />
-            </mt-tab-container-item>
-            <mt-tab-container-item id="发现">
-                <mt-cell :title="发现" />
-            </mt-tab-container-item>
-            <mt-tab-container-item id="我的">
-                123
-                <router-link to="/">
-                    <!-- button -->
-                    <mt-button type="danger" size="large">退出</mt-button>
-                </router-link>
-            </mt-tab-container-item>
+
         </mt-tab-container>
     </div>
 
