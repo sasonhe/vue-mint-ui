23c056e4174606f88f8d9810b556faaec45b8724
sasonhe <642139009@qq.com>
2017-10-19 20:29:35 +0800

    delete

--- src/components/header.vue
+++ src/components/header.vue
@@ -1,20 +1,20 @@
 <template>
 <div class="">
     <mt-header title="首页">
-      <router-link to="/" slot="left">
-        <mt-button icon="back">返回</mt-button>
-      </router-link>
-      <mt-button icon="more" slot="right"></mt-button>
+        <router-link to="/" slot="left">
+            <mt-button icon="back">返回</mt-button>
+        </router-link>
+        <mt-button icon="more" slot="right"></mt-button>
     </mt-header>
 </div>
 </template>
 <script>
-export default{
-    components:{
+export default {
+    components: {
 
     }
 }
 </script>
-<style scoped>
+<style>
 
 </style>
--- src/components/swipe.vue
+++ src/components/swipe.vue
@@ -1,46 +1,47 @@
 <template>
-    <div class="">
-        <mt-swipe :auto="4000" class="wrap">
-          <mt-swipe-item class="ban1" v-for="item in imgData"><img :src="item.imgUrl" alt=""></mt-swipe-item>
+<div class="">
+    <mt-swipe :auto="4000" class="wrap">
+        <mt-swipe-item class="ban1" v-for="item in imgData"><img :src="item.imgUrl" alt=""></mt-swipe-item>
 
-        </mt-swipe>
-    </div>
+    </mt-swipe>
+</div>
 </template>
 
 <script>
-  export default {
+export default {
     data() {
-      return {
-          imgData : [
-              {
-                  imgUrl : 'http://c.hiphotos.baidu.com/image/pic/item/f2deb48f8c5494ee321ef3d824f5e0fe99257e37.jpg'
-              },
-              {
-                  imgUrl : 'http://c.hiphotos.baidu.com/image/pic/item/024f78f0f736afc333faafa5ba19ebc4b645127f.jpg'
-              },
-              {
-                  imgUrl : 'http://e.hiphotos.baidu.com/image/pic/item/b999a9014c086e065195c08a0b087bf40ad1cb36.jpg'
-              }
-          ]
-      }
+        return {
+            imgData: [{
+                    imgUrl: 'http://c.hiphotos.baidu.com/image/pic/item/f2deb48f8c5494ee321ef3d824f5e0fe99257e37.jpg'
+                },
+                {
+                    imgUrl: 'http://c.hiphotos.baidu.com/image/pic/item/024f78f0f736afc333faafa5ba19ebc4b645127f.jpg'
+                },
+                {
+                    imgUrl: 'http://e.hiphotos.baidu.com/image/pic/item/b999a9014c086e065195c08a0b087bf40ad1cb36.jpg'
+                }
+            ]
+        }
     },
-    created() {
-    },
-    methods: {
-    }
-  }
+    created() {},
+    methods: {}
+}
 </script>
 
-<style scoped>
-.wrap{
+<style>
+.wrap {
     height: 100px;
     text-align: center;
 }
+
+
 /*.ban1,.ban2,.ban3{
     width: 100%;
     height: 100px;
     line-height: 100px;
 }*/
+
+
 /*.ban1{
     background: #555;
 }
@@ -50,7 +51,8 @@
 .ban3{
     background: #ccc;
 }*/
-img{
+
+img {
     width: 100%;
     height: 100%;
 }
